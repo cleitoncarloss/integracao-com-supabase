414f69b2782470af97630f0737c45b3d4dfd930a
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2024-02-27 23:56:19 -0300

    refactor: modifica o nome da variavel e exporta ela

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient('https://gyezcramcvtfpcwcwbzt.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5ZXpjcmFtY3Z0ZnBjd2N3Ynp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2NDY5MzUsImV4cCI6MjAyNDIyMjkzNX0.789_Mu3PGfJ5kIXl2x_1Bp76M6Cs54mmch-n1FQZrFw')
 
-let { data, error } = await supabase
+let { data:user, error } =  await supabase
   .from('users')
   .select('*')
 
@@ -10,4 +10,4 @@ if (error) {
   console.log(error)
 }
 
-console.log(data)
+export default user
